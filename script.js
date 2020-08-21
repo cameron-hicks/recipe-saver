86384f51dbd01e274d626bf5ac6d18e1e04461c4
Cameron Baumgartner <cameron@Camerons-MacBook-Air.local>
2020-08-21 00:15:53 -0400

    Fixed flexbox stylings.

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-//const recipe = { title: "Black Bean Nachos", body: "recipe here" };
-//I'll use this function to make a recipe object for each user-generated recipe, where title and body are variables whose values are set by strings intered in input and textarea elements where the user types their recipe's title and instructions.
-
-const recipe = {
-    title,
-    ingredients,
-    instructions
+let recipe = {
+    title: "", //string
+    vegetarian: false, //boolean
+    spicy: false, //boolean
+    healthy: false, //boolean
+    weeknight: false, //boolean
+    servings: "", //string
+    time: "", //string
+    ingredients: ``, //string
+    instructions: `` //string
 };
 
 console.log(recipe);
+
+function makeRecipeObject() {
+    return {
+        title: document.getElementById("title"),
+        vegetarian: document.getElementById("vegetarian"),
+        spicy: document.getElementById("spicy"),
+        healthy: document.getElementById("healthy"),
+        weeknight: document.getElementById("weeknight"),
+        servings: document.getElementById("servings"),
+        time: document.getElementById("time"),
+        ingredients: document.getElementById("ingredients"),
+        instructions: document.getElementById("instructions")
+    };
+}
+
+const newRecipe = document.getElementById("submit").addEventListener("click", makeRecipeObject()); //there's a problem with this line
+console.log(newRecipe);
+localStorage.setItem(newRecipe.title, JSON.stringify(newRecipe)); //error: Cannot read property 'title' of undefined at script.js:31
+
 
 // title.addEventListener("keyup", (event) => {
 //     //console.log(event.currentTarget);
@@ -15,40 +37,30 @@ console.log(recipe);
 //     localStorage.setItem("title", title.value);
 //     localStorage.setItem("instructions", instructions.value);
 //     });
+// function readLastRecipe() {
+//     
+// }
 
-function makeRecipeObject() {
-    return {
-        title = document.getElement("title"), //is it okay that the property and the variable have the same name?
-        ingredients = document.getElement("ingredients"),
-        instructions = document.getElement("instructions")
-    };
-}
+// readRecentRecipe();
+// console.log(recipe);
+// recipe = makeRecipeObject();
+// console.log(recipe);
 
-function readLastRecipe() {
-    localStorage.setItemById("readTitle", recipe.title);
-    localStorage.setItemById("readInstructions", recipe.instructions);
-}
+// function storeRecipeInLocalStorage(recipe) {
+// 	//takes a recipe object
+// 	//stores it in local storage
+// 	console.log(recipe);
+// }
 
-readRecentRecipe();
-console.log(recipe);
-recipe = makeRecipeObject();
-console.log(recipe);
+// storeRecipeInLocalStorage(recipe);
+// console.log(recipe);
 
-function storeRecipeInLocalStorage(recipe) {
-	//takes a recipe object
-	//stores it in local storage
-	console.log(recipe);
-}
-
-storeRecipeInLocalStorage(recipe);
-console.log(recipe);
-
-function readRecipeFromLocalStorage(title) {
-	//prints out in html the recipe object saved in local storage with the corresponding title
-	//get the recipe object with the passed title: localStorage.getItem(....).... 
-	localStorage.setItemById("readTitle", recipe.title);
-	localStorage.setItemById("readInstructions", recipe.instructions);
-}
+// function readRecipeFromLocalStorage(title) {
+// 	//prints out in html the recipe object saved in local storage with the corresponding title
+// 	//get the recipe object with the passed title: localStorage.getItem(....).... 
+// 	localStorage.setItemById("readTitle", recipe.title);
+// 	localStorage.setItemById("readInstructions", recipe.instructions);
+// }
 
 //readRecipeFromLocalStorage("Black Bean Nachos");
 
