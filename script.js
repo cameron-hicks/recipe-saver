941522ca9a2871292d29f0e6672e05f3736e2665
Cameron Baumgartner <cameron@Camerons-MacBook-Air.local>
2020-09-21 22:55:03 -0400

    Refactored for custom save() & load(). Reset funct

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-//TO-DO: make manual save() and load() functions. Instead of adding event listeners and getting and setting items in local storage with one line of code and a loop that iterates through an array holding all of the form elements, I'll need to handle each form element individually, since the checkboxes are booleans and need to be handled in a different way from the string inputs. Scroll towards end of file for some work I've already done towards this refactoring. https://stackoverflow.com/questions/26628812/localstorage-how-to-save-a-checkbox
-
-// Add event listeners so that on keyup the user's progress in each form element will be saved in local storage.
-/*
-const title = document.getElementById("title");
-title.addEventListener("keyup", event => {
-    localStorage.setItem("title", title.value);
-    console.log("Saved in local storage: " + title);
-});
-*/
-
-// Initialize an array and fill it with the html form elements.
-
-
-// console.log(formElements);
-
 const submitButton = document.getElementById("submit");
+const resetButton = document.getElementById("reset");
+const arrayOfRecipes = getAllStorage();
 
+//When user clicks the submit button, save the data they've entered as an object with key-value pairs corresponding to the input elements and the values entered into them.
 submitButton.addEventListener("click", event => {
     const inputtedValues = {
         title: document.getElementById("title").value,
@@ -28,31 +15,21 @@ submitButton.addEventListener("click", event => {
         ingredients: document.getElementById("ingredients").value,
         instructions: document.getElementById("instructions").value
     };
-    // console.log(formElements);
+
+    // The "debugger" statement on the line below allows you to pause the code's running in dev tools if there's an error and view what's been done so far up to the point of the error.
     // debugger
+
     // When you need to access this object again, you can do so by referencing its title property with dot notation.
-    localStorage.setItem(`${inputtedValues.title}`, JSON.stringify(inputtedValues));
+    // localStorage.setItem(`${inputtedValues.title}`, JSON.stringify(inputtedValues));
+    save(inputtedValues);
 
     location.reload();
 });
 
-// console.log(JSON.parse(localStorage.getItem("Black Bean Nachos")));
-
-// custom function to get all recipe objects stored in the localStorage object
-function getAllStorage() {
-
-    const recipes = [];
-    const keys = Object.keys(localStorage);
-
-    keys.forEach(key => {
-        recipes.push(JSON.parse(localStorage.getItem(key)))
-    });
-
-    return recipes;
-}
-
-
-const arrayOfRecipes = getAllStorage();
+//When user clicks the reset button, refresh the page.
+resetButton.addEventListener("click", event => {
+    location.reload();
+});
 
 arrayOfRecipes.forEach(recipe => {
     // Use JQuery to populate the <ul> element on sidebar div of index.html with the titles of all recipe objects saved in localStorage
@@ -60,3 +37,28 @@ arrayOfRecipes.forEach(recipe => {
     $("#recipeList").append(`<li><a href="#">${recipe.title}</a></li>`);
 })
 
+// custom function to save a recipe to localStorage. Accepts a recipe object. Returns nothing.
+function save(recipe){
+    localStorage.setItem(`${recipe.title}`, JSON.stringify(recipe));
+}
+
+// custom function to load a recipe from localStorage. Accepts a string corresponding to the title of the recipe. Returns an object.
+function load(title){
+    return JSON.parse(localStorage.getItem(title));
+}
+
+// custom function to get all recipe objects stored in the localStorage object. Returns an array of objects.
+function getAllStorage() {
+
+    // declare a variable which will hold an array of recipe objects
+    const recipes = [];
+    //declare an array which will hold strings representing the titles of all the recipes stored in localStorage
+    const titlesArray = Object.keys(localStorage);
+
+    //iterate through the array of recipe titles and pass each title to .push() to populate the array of recipe objects with recipe objects 
+    titlesArray.forEach(title => {
+        recipes.push(load(title));
+    });
+
+    return recipes;
+}
