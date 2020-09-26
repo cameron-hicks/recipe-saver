256702c2bc60fff899bea0c6a5cbe54fe4a5b79e
Cameron Baumgartner <cameron@Camerons-MacBook-Air.local>
2020-09-26 11:01:56 -0400

    Working on display recipe functionality

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ const submitButton = document.getElementById("submit");
 const resetButton = document.getElementById("reset");
 const arrayOfRecipes = getAllStorage();
 
-//When user clicks the submit button, save the data they've entered as an object with key-value pairs corresponding to the input elements and the values entered into them. Refresh the page.
+//TODO: Saved recipe objects are not appending to the corresponding <ul> on /Display/index.html, only on /index.html.
+// Use JQuery to populate the <ul> element on sidebar div of index.html with the titles of all recipe objects saved in localStorage. Will execute on page load.
+arrayOfRecipes.forEach(recipe => {
+    console.log(recipe);
+    $("#recipeList").append(`<li><a href="/Display/index.html">${recipe.title}</a></li>`);
+});
+
+try{
+//When user clicks the submit button, save the data they've entered as an object with key-value pairs corresponding to the input elements and the values entered into them. Append the new recipe title to the <ul> of recipe titles on the html pages. Refresh the page.
 submitButton.addEventListener("click", event => {
     
     //Declare a variable to hold an array of all the ids of input elements whose values you want to use to make a recipe object.
@@ -22,20 +30,60 @@ submitButton.addEventListener("click", event => {
     // localStorage.setItem(`${inputtedValues.title}`, JSON.stringify(inputtedValues));
     save(inputtedValues);
 
+
     //Refresh the page.
     location.reload();
 });
 
-//When user clicks the reset button, refresh the page.
+
+//When user clicks the reset button, refresh the page. Don't save data.
 resetButton.addEventListener("click", event => {
     location.reload();
 });
+}
+catch(err) {
+    console.log("No submit or reset button on this page.");
+}
 
-arrayOfRecipes.forEach(recipe => {
-    // Use JQuery to populate the <ul> element on sidebar div of index.html with the titles of all recipe objects saved in localStorage
-    console.log(recipe);
-    $("#recipeList").append(`<li><a href="/Recipe/index.html">${recipe.title}</a></li>`);
-})
+//When user clicks an <a> element in the sidebar, load /Display/index.html and invoke loadDisplay, passing it the recipe object that has a title property matching the anchor text.
+//Wrap in a try-catch. If there is no recipe object corresponding to the anchor text, it'll show an erorr message.
+try {
+    arrayOfObjects.forEach(obj => {
+        const anchor = document.getElementById(obj.title);
+        anchor.addEventListener("click", event => {
+            loadDisplay(obj.title);
+        });
+    });
+
+    /*
+    //dummy variable to stand in for anchor elements
+    const anchor = document.getElementById("anchorA");
+    anchor.addEventListener("click", event => {
+        //TODO: What attributes does an anchor element have? I want to try accessing using dot notation an attribute that will return the anchor text.
+        const recipeTitle = anchor.value;
+        console.log(recipeTitle);
+        //Load the recipe title from localStorage using my custom load function, which accepts a string corresponding to the object's title property.
+        const recipe = load(recipeTitle);
+        loadDisplay(recipeTitle);
+    });
+    */
+}
+catch(err) {
+    console.log("Error attaching event listeners to anchors");
+}
+
+//Accepts a string corresponding to the title property of a recipe object. Populates the <p>s on /Display/index.html with the recipe object's values.
+function loadDisplay(recipeTitle){
+    //load the recipe object with the matching title property
+    const recipe = load(recipeTitle);
+
+    document.getElementById("displayTitle").value = recipe.title;
+    document.getElementById("displayServings").value = recipe.servings;
+    document.getElementById("displayTime").value = recipe.time;
+    document.getElementById("displayIngredients").value = recipe.ingredients;
+    document.getElementById("displayInstructions").value = recipe.instructions;
+}
+
 
 // custom function to save a recipe to localStorage. Accepts a recipe object. Returns nothing.
 function save(recipe){
